3b8183cd836eb816d74b1058b450e9d0beacd553
Ayush Raj <95844463+AyushRaj00@users.noreply.github.com>
2022-07-18 15:03:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+//REACT_APP_TOM_TOM_API_KEY= AxAqNVqDN85TaCUvv4D30ot15NhT1E4P
